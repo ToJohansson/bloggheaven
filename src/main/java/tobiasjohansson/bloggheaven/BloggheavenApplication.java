59f778ede119eb63b96df2caf4a71157e2f220a7
Tobias Johansson <tobiasp91@hotmail.com>
2023-06-08 09:56:38 +0200

    initial commit

---
+++ src/main/java/tobiasjohansson/bloggheaven/BloggheavenApplication.java
@@ -0,0 +1,18 @@
+package tobiasjohansson.bloggheaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggheavenApplication {
+
+    /**
+     *
+     * Author: Tobias Johansson
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggheavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tobiasjohansson/bloggheaven/BloggheavenApplicationTests.java
@@ -0,0 +1,13 @@
+package tobiasjohansson.bloggheaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggheavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
